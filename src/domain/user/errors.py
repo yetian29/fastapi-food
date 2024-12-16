61b740a6d5dbc8738eb41da6121f6e462f55ca77
yetian29 <dathien000@gmail.com>
2024-12-16 10:36:48 +0700

    Initial

--- src/domain/user/errors.py
+++ src/domain/user/errors.py
@@ -4,27 +4,24 @@ from src.domain.base.errors import BaseDomainException
 class BaseUserException(BaseDomainException):
     pass
 
-
 class UserIsNotFoundException(BaseUserException):
     pass
-
 
 class UserIsExsitedException(BaseUserException):
     pass
 
-class CodeException(BaseUserException):
-    pass
-
 class CrendentialUserException(BaseUserException):
     pass
-
 
 class PasswordInvalidException(CrendentialUserException):
     pass
 
 class OldPasswordInCorrectException(CredentialUserException):
     pass
-
+    
+class CodeException(BaseUserException):
+    pass
+    
 class DataVerifyAreNotFoundException(CodeException):
     pass
 
